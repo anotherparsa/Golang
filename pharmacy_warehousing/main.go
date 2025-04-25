6f7e3aa77386868cf19eaf24660319255dc5acd3
anotherparsa <anotherparsa@gmail.com>
2025-04-25 20:04:33 +0330

    products route

--- pharmacy_warehousing/main.go
+++ pharmacy_warehousing/main.go
@@ -7,7 +7,9 @@ import (
 
 func main() {
 	http.HandleFunc("/", HomePageHandler)
-
+	http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This is the product page")
+	})
 	fmt.Println("Starting server on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
