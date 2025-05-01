17158ffec24286c80ae307670cb7058d848d5f9a
anotherparsa <anotherparsa@gmail.com>
2025-05-01 23:27:24 +0330

    learning to work with mysql

--- test/main.go
+++ test/main.go
@@ -7,14 +7,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-const (
-	db_user     = "testuser"
-	db_password = "rrc3498urc38r9j999m8j"
-	db_address  = "127.0.0.1"
-	db_port     = "3306"
-	db_name     = "pharmacywarehouse"
-)
-
 type Staff struct {
 	Id       string
 	Name     string
@@ -22,31 +14,95 @@ type Staff struct {
 	Position string
 }
 
+const (
+	dbUser     = "testuser"
+	dbPassword = "rrc3498urc38r9j999m8j"
+	dbAddress  = "127.0.0.1"
+	dbPort     = "3306"
+	dbName     = "pharmacywarehouse"
+)
+
 func main() {
-	insert_data("testn1", "testf1", "testp1")
-	insert_data("testn2", "testf2", "testp2")
-	insert_data("testn3", "testf3", "testp3")
-	insert_data("testn4", "testf4", "testp4")
+	create("n1", "f1", "p1")
+	create("n2", "f2", "p2")
+	create("n3", "f3", "p3")
+	create("n4", "f4", "p4")
+
+	for i, v := range readAll() {
+		fmt.Printf("Instance %v the array is %v \n", i, v)
+	}
 }
 
-func connect() *sql.DB {
-	database, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", db_user, db_password, db_address, db_port, db_name))
+func connectToDatabase() (*sql.DB, error) {
+	dataBase, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbAddress, dbPort, dbName))
 
 	if err != nil {
-		fmt.Printf("Error connecting database %v\n", err)
+		fmt.Printf("Error in connecting to the database %v\n", err)
 	}
 
-	return database
+	return dataBase, err
+
 }
 
-func insert_data(name string, family string, position string) {
-	database := connect()
-
-	querry, _ := database.Prepare("INSERT INTO `staff` (name, family, position) VALUES (?, ?, ?)")
-
-	_, err := querry.Exec(name, family, position)
+// crud
+// create
+func create(name string, family string, position string) {
+	dataBase, err := connectToDatabase()
 
 	if err != nil {
-		fmt.Printf("Error executing the query %v\n", err)
+		fmt.Printf("Failed to connect to the database %v\n", err)
 	}
+
+	defer dataBase.Close()
+
+	querry, err := dataBase.Prepare("INSERT INTO staff (name, family, position) VALUES (?, ?, ?)")
+
+	if err != nil {
+		fmt.Printf("Error in preparing the querry %v\n", err)
+	}
+
+	defer querry.Close()
+
+	_, err = querry.Exec(name, family, position)
+
+	if err != nil {
+		fmt.Printf("Error executing the querry %v\n", err)
+	}
+}
+
+// read
+func readAll() []Staff {
+	dataBase, err := connectToDatabase()
+
+	if err != nil {
+		fmt.Printf("Failed to connect to the database %v\n", err)
+	}
+
+	defer dataBase.Close()
+
+	rows, err := dataBase.Query("SELECT * FROM staff")
+
+	if err != nil {
+		fmt.Printf("Error in querrying %v\n", err)
+	}
+
+	defer rows.Close()
+
+	staffsArray := []Staff{}
+	staffInstance := Staff{}
+	for rows.Next() {
+		err = rows.Scan(&staffInstance.Id, &staffInstance.Name, &staffInstance.Family, &staffInstance.Position)
+
+		if err != nil {
+			fmt.Printf("Error in scanning rows %v\n", err)
+			continue
+		}
+		staffsArray = append(staffsArray, staffInstance)
+	}
+
+	if rows.Err() != nil {
+		fmt.Printf("Error during the iteration %v\n", rows.Err())
+	}
+
+	return staffsArray
 }
