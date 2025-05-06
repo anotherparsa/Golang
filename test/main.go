24d8bf82ad32a3f7604636eb5ac23f68e7f0298d
anotherparsa <anotherparsa@gmail.com>
2025-05-06 18:01:17 +0330

    learning sql with go

--- test/main.go
+++ test/main.go
@@ -7,80 +7,78 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+const (
+	db_user     = "testuser"
+	db_password = "rrc3498urc38r9j999m8j"
+	db_address  = "127.0.0.1"
+	db_port     = "3306"
+	db_name     = "pharmacywarehouse"
+)
+
 type Staff struct {
 	Id       string
 	Name     string
 	Family   string
+	Staffid  string
 	Position string
+	password string
 }
-
-const (
-	dbUser     = "testuser"
-	dbPassword = "rrc3498urc38r9j999m8j"
-	dbAddress  = "127.0.0.1"
-	dbPort     = "3306"
-	dbName     = "pharmacywarehouse"
-)
 
 func main() {
-	create("n1", "f1", "p1")
-	create("n2", "f2", "p2")
-	create("n3", "f3", "p3")
-	create("n4", "f4", "p4")
+	insert_staff("testname1", "testfamily1", 1, "testpos1", "testpassword1")
+	insert_staff("testname2", "testfamily2", 2, "testpos2", "testpassword2")
+	insert_staff("testname3", "testfamily3", 3, "testpos3", "testpassword3")
+	insert_staff("testname4", "testfamily4", 4, "testpos4", "testpassword4")
 
-	for i, v := range readAll() {
-		fmt.Printf("Instance %v the array is %v \n", i, v)
+	for i, v := range read_all_staff() {
+		fmt.Printf("The index is %v and the value is %v\n", i, v)
 	}
 }
 
-func connectToDatabase() (*sql.DB, error) {
-	dataBase, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbAddress, dbPort, dbName))
+func connect() (*sql.DB, error) {
+	database, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", db_user, db_password, db_address, db_port, db_name))
 
 	if err != nil {
-		fmt.Printf("Error in connecting to the database %v\n", err)
+		fmt.Printf("Error connecting to the database %v\n", err)
 	}
 
-	return dataBase, err
-
+	return database, err
 }
 
-// crud
-// create
-func create(name string, family string, position string) {
-	dataBase, err := connectToDatabase()
+func insert_staff(name string, family string, staffid int, positions string, password string) {
+	database, err := connect()
 
 	if err != nil {
-		fmt.Printf("Failed to connect to the database %v\n", err)
+		fmt.Printf("Failed to connect to the database: %v\n", err)
 	}
 
-	defer dataBase.Close()
+	defer database.Close()
 
-	querry, err := dataBase.Prepare("INSERT INTO staff (name, family, position) VALUES (?, ?, ?)")
+	querry, err := database.Prepare("INSERT INTO staff (name, family, staffid, position, password) VALUES (?, ?, ?, ?, ?)")
 
 	if err != nil {
-		fmt.Printf("Error in preparing the querry %v\n", err)
+		fmt.Printf("Error preparing the querry: %v\n", err)
 	}
 
 	defer querry.Close()
 
-	_, err = querry.Exec(name, family, position)
+	_, err = querry.Exec(name, family, staffid, positions, password)
 
 	if err != nil {
-		fmt.Printf("Error executing the querry %v\n", err)
+		fmt.Printf("Error executing the querry: %v\n", err)
 	}
 }
 
-// read
-func readAll() []Staff {
-	dataBase, err := connectToDatabase()
+func read_all_staff() []Staff {
+	database, err := connect()
 
 	if err != nil {
-		fmt.Printf("Failed to connect to the database %v\n", err)
+		fmt.Printf("Failed to connect to the database: %v\n", err)
 	}
 
-	defer dataBase.Close()
+	defer database.Close()
 
-	rows, err := dataBase.Query("SELECT * FROM staff")
+	rows, err := database.Query("SELECT * FROM staff")
 
 	if err != nil {
 		fmt.Printf("Error in querrying %v\n", err)
@@ -88,21 +86,24 @@ func readAll() []Staff {
 
 	defer rows.Close()
 
-	staffsArray := []Staff{}
 	staffInstance := Staff{}
+	staffArray := []Staff{}
+
 	for rows.Next() {
-		err = rows.Scan(&staffInstance.Id, &staffInstance.Name, &staffInstance.Family, &staffInstance.Position)
+		err = rows.Scan(&staffInstance.Id, &staffInstance.Name, &staffInstance.Family, &staffInstance.Staffid, &staffInstance.Position, &staffInstance.password)
 
 		if err != nil {
-			fmt.Printf("Error in scanning rows %v\n", err)
+			fmt.Printf("Error in scanning the rows %v\n", err)
 			continue
 		}
-		staffsArray = append(staffsArray, staffInstance)
+
+		staffArray = append(staffArray, staffInstance)
 	}
 
 	if rows.Err() != nil {
 		fmt.Printf("Error during the iteration %v\n", rows.Err())
 	}
 
-	return staffsArray
+	return staffArray
+
 }
