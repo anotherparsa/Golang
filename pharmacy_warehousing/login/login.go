076da588e740d773ea18cc1cf7857637de2d67e4
anotherparsa <anotherparsa@gmail.com>
2025-05-14 00:13:48 +0330

    Serving static files

--- pharmacy_warehousing/login/login.go
+++ pharmacy_warehousing/login/login.go
@@ -16,12 +16,11 @@ func Login_page(w http.ResponseWriter, r *http.Request) {
 
 func Login_processor(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-
 	staff, err := staff.Get_staff(r.PostForm.Get("staffid"), r.PostForm.Get("password"))
 
 	if err != nil {
 		fmt.Printf("Failed to get the staff : %v\n", err)
-		http.Redirect(w, r, "/login", 302)
+		http.Redirect(w, r, "/staff/login", 302)
 	} else {
 		new_uuid := uuid.New().String()
 		session.Set_session(w, new_uuid, staff.Userid)
--- pharmacy_warehousing/main.go
+++ pharmacy_warehousing/main.go
@@ -8,6 +8,9 @@ import (
 
 func main() {
 	http.HandleFunc("/", router.Routing)
+
+	//serving static files
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	fmt.Println("Starting server on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
